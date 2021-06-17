22320d723ad985b3b849865a8748056cb5405182
shaftsyy <85958758+shaftsyy@users.noreply.github.com>
2021-06-17 22:28:11 +0700

    Update SMART MAP FTUI.c

--- SMART MAP FTUI.c
+++ SMART MAP FTUI.c
@@ -269,7 +269,6 @@ void printMap(){
     printf("\n\t\t\t\t\t--------------------------------------------");
     printf("\n\t\t\t\t\tKeterangan:");
     printf("\n\t\t\t\t\tX: lokasi saat ini");
-    printf("\n\t\t\t\t\t-: rute");
     printf("\n\t\t\t\t\tO: jalan");
     printf("\n\t\t\t\t\t#: bukan jalan");
     printf("\n\t\t\t\t\t--------------------------------------------");
